3bd89952efb1989e5a3aa2e48579b4b811d89a1c
Ashok Kumar <k.ashok502@gmail.com>
2012-10-16 16:57:23 +0530

    small changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,17 @@ WebApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { :host => 'http://ancient-anchorage-2110.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'ancient-anchorage-2110.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    #:domain               => 'baci.lindsaar.net',
+    :user_name            => 'k.ashok502@gmail.com',
+    :password             => 'ashokkumar',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
