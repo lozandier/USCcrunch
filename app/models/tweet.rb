eaf6550fdda19c5092e7ee25aeb3ad20ce01e7fe
Ashok Kumar <k.ashok502@gmail.com>
2012-11-14 18:51:24 +0530

    small changes

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,17 +2,6 @@ class Tweet < ActiveRecord::Base
   belongs_to :user
   attr_accessible :user_id,:receiver_id, :body,:document,:reply,:tweet_id
   validates :body, :presence => true
-#  has_attached_file :document, :styles => {:medium => "300x300>", :thumb => "100x100>"}
-  has_attached_file :document,
-    :default_url => '/assets/avatar.png',
-    :whiny => false,
-    :storage => :s3,
-    :s3_credentials => "#{Rails.root}/config/s3.yml",
-    :path => "uploaded_files/profile/:id/:style/:basename.:extension",
-    :bucket => S3_BUCKET_NAME,
-    :styles => {
-    :original => "300x300>",
-    :default => "280x190>",
-    :other => "100x100>" }
+  has_attached_file :document, :styles => {:medium => "300x300>", :thumb => "100x100>"}
   has_one :favorite, :dependent => :destroy
 end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,1 @@
 SITE= 'http://ancient-anchorage-2110.herokuapp.com'
-S3_BUCKET_NAME = "mdih_staging"
