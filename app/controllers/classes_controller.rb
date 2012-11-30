936ea812ba73f8302d11979dd4f1cce6671c0607
Ashok Kumar <k.ashok502@gmail.com>
2012-11-30 18:02:00 +0530

    small changes

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -9,7 +9,7 @@ class ClassesController < ApplicationController
   def show
     @user = User.find(params[:id])
     @post = @user.tweets.new(params[:tweet])
-    sql_query = "select * from tweets a where (a.user_id in (select  user_id from tweets where receiver_id=#{current_user.id})) and (id in (select max(id) from tweets b where a.user_id = b.user_id and a.receiver_id = b.receiver_id)) and  a.receiver_id = #{current_user.id} order by a.created_at desc"
+    sql_query = "select * from tweets a where (id in (select max(id) from tweets b where a.user_id = b.user_id )) order by a.created_at desc"
     @posts = Tweet.paginate_by_sql [sql_query], :per_page => 10, :page => params[:page]
     respond_to do |format|
       format.html {render :partial => "show", :layout => false if request.xhr?}
