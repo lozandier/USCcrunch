7e4496efcf9509c1d7783477a838b0da5900dd34
Ashok Kumar <k.ashok502@gmail.com>
2012-12-07 17:59:20 +0530

    change the conversation form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -37,7 +37,7 @@ class ProfilesController < ApplicationController
   def conversation
     @user = User.find(params[:id])
     @post = @user.tweets.new(params[:tweet])
-    @posts = Tweet.where("(user_id = '#{@user.id}' or receiver_id = '#{@user.id}') and reply IS NULL").order("created_at Asc").paginate :page => params[:page], :per_page => 10
+    @posts = Tweet.where("reply IS NOT NULL or reply IS NULL").order("created_at Asc")
     render :layout => false
   end
 
