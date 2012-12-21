7c0db38f59ab9d98c7ba45c02adc7a8a719cafcf
Ashok Kumar <k.ashok502@gmail.com>
2012-12-21 16:45:30 +0530

    delete the unused data

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -47,6 +47,10 @@ class ProfilesController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user.id != current_user.id
+      flash[:error] = 'Access Denied.'
+      redirect_to profile_path(current_user)
+    end
   end
 
   def update
