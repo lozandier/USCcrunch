19c357f679022f16b495b51dcb1995378956b8a5
Ashok Kumar <k.ashok502@gmail.com>
2013-01-05 16:42:07 +0530

    add the chart

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -25,63 +25,15 @@ class ClassesController < ApplicationController
 
   def graphs
     @user = User.find(params[:id])
-    #@graph = open_flash_chart_object(750,400,"/classes/#{@user.id}/graph")
-    @graphs = open_flash_chart_object(750,300,"/classes/#{@user.id}/graph_code")
-  end
-
-  def graph
-    @user = User.find(params[:id])
-    bar_v = []
-    title = Title.new("No Of Students")
-    bar = BarGlass.new
-    year = Time.now.year
-    i = Time.now.strftime("%m").to_i
-    y_range = []
-    (1..31).each do |j|
-      xx = "#{year}-#{i}-#{j}".to_time
-      y= User.where("role = 'student' and reset_password_token IS NULL and users.created_at LIKE '#{xx.to_datetime.strftime('%Y-%m-%d')}%'").count
-      bar_values = BarValue.new(y);
-      y_range << y
-      bar_v << bar_values
+    @year = Time.now.strftime("%Y").to_i
+    @year1 = @year-1
+    @year2 = @year1-1
+    @year3 = @year2-1
+    @year4 = @year3-1
+    @year5 = @year4-1
+    respond_to do |format|
+      format.js
     end
-    bar.set_values(bar_v)
-    x = XAxis.new
-    days = Time.days_in_month(i)
-    x.labels =  (1..days).to_a.collect(&:to_s)
-    range = y_range.sort().last > 15 ? y_range.sort().last : 15
-    y_ax = YAxis.new
-    y_ax.set_range(0,range,1)
-    chart = OpenFlashChart.new
-    chart.set_title(title)
-    chart.bg_colour='#F9F9F9'
-    chart.add_element(bar)
-    chart.x_axis = x
-    chart.y_axis = y_ax
-    render :text => chart
-  end
-
-  def graph_code
-    @user = User.find(params[:id])
-    # based on this example - http://teethgrinder.co.uk/open-flash-chart-2/pie-chart.php
-    title = Title.new("Students And Posts")
-    @total_received_people = User.where("id = #{@user.id} and role = 'student'").count
-    @total_received_post = Tweet.where("user_id = #{@user.id} and reply IS NULL").count
-
-    pie = Pie.new
-    pie.start_angle = 90
-    pie.animate = true
-    pie.tooltip = '#val# of #total#<br>#percent# of 100%'
-    pie.colours = ["#d01f3c","#d01fff"]
-    pie.values  = [PieValue.new(@total_received_people,"Students"),PieValue.new(@total_received_post,"Posts")] #[@total_non_replied_people, @total_replied_people]
-
-    chart = OpenFlashChart.new
-    chart.title = title
-    chart.bg_colour='#F9F9F9'
-    chart.add_element(pie)
-
-    chart.x_axis = nil
-
-    render :text => chart.to_s
   end
 
   def edit
