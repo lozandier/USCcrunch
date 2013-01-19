36ef86332ececb8f80e1d3d333018e5e28dd41fb
Ashok Kumar <k.ashok502@gmail.com>
2013-01-19 14:53:40 +0530

    small changes

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -42,7 +42,6 @@ WebApp::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
-  config.assets.compile = true
 
   # Expands the lines which load the assets
   config.assets.debug = true
